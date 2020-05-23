6b7a0b7b312501b1f3c76ef8c1bbdff222fcb0d6
collin9726 <owino.collin@gmail.com>
2020-05-23 16:34:12 +0300

    add tests for add credential and create credential methods

--- app_test.py
+++ app_test.py
@@ -3,11 +3,13 @@ import pyperclip
 from account import Account
 from password import Password
 from user import User
+from credentials import Credentials
 
 class AppTest(unittest.TestCase):
     def setUp(self):
         self.account_obj=Account("Twitter", "collin", "collin123")
         self.user_obj=User("collin", "collin123")
+        self.credentials_obj=Credentials()
 
     def tearDown(self):
         User.users_list=[]
@@ -24,27 +26,39 @@ class AppTest(unittest.TestCase):
     # def test_gen_password_copy(self):
     #     self.assertEqual(Password.gen_password(), pyperclip.paste())  
 
-    def test_user_init(self):
-        self.assertEqual(self.user_obj.username, "collin")
-        self.assertEqual(self.user_obj.password, "collin123")
+    # def test_user_init(self):
+    #     self.assertEqual(self.user_obj.username, "collin")
+    #     self.assertEqual(self.user_obj.password, "collin123")
 
-    def test_add_user(self):
-        self.user_obj.add_user(self.user_obj)
-        self.assertEqual(len(User.users_list),1)
+    # def test_add_user(self):
+    #     self.user_obj.add_user(self.user_obj)
+    #     self.assertEqual(len(User.users_list),1)
 
-    def test_add_multiple_users(self):
-        self.user_obj.add_user(self.user_obj)
-        other_user_object=User("John", "doe123")
-        other_user_object.add_user(other_user_object)      
-        self.assertEqual(len(User.users_list),2)
+    # def test_add_multiple_users(self):
+    #     self.user_obj.add_user(self.user_obj)
+    #     other_user_object=User("John", "doe123")
+    #     other_user_object.add_user(other_user_object)      
+    #     self.assertEqual(len(User.users_list),2)
 
-    def test_check_login(self):
-        self.user_obj.add_user(self.user_obj)
-        self.assertTrue(self.user_obj.check_login("collin", "collin123"))
+    # def test_check_login(self):
+    #     self.user_obj.add_user(self.user_obj)
+    #     self.assertTrue(self.user_obj.check_login("collin", "collin123"))
 
-    def test_return_user(self):
-        self.user_obj.add_user(self.user_obj)
-        self.assertEqual(User.return_user("collin", "collin123"), self.user_obj)
+    # def test_return_user(self):
+    #     self.user_obj.add_user(self.user_obj)
+    #     self.assertEqual(User.return_user("collin", "collin123"), self.user_obj)
+
+    # def test_add_credential(self):
+    #     self.credentials_obj.add_credential()
+    #     self.assertEqual(len(self.credentials_obj.credentials_list),1)
+
+    def test_create_credential_sys_password(self):
+        self.credentials_obj.create_credential()
+        self.assertEqual(len(self.credentials_obj.credentials_list),1) 
+
+    def test_create_credential_custom_password(self):
+        self.credentials_obj.create_credential()
+        self.assertEqual(len(self.credentials_obj.credentials_list),1)        
 
 
 if __name__=='__main__':
