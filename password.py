96852f6d37ca573ac6e829a785a15ef042371a07
collin9726 <owino.collin@gmail.com>
2020-05-23 12:04:29 +0300

    add functionality to allow user to input length of sys password

--- password.py
+++ password.py
@@ -12,7 +12,21 @@ class Password:
 
     @classmethod
     def gen_password(cls):
-        sys_password="".join(random.sample(cls.password_chars, k=10))
+        pass_length=10
+        num_valid=True
+        while num_valid:
+            try:
+                pass_length=int(input("Enter password length (at least 5): "))
+                if pass_length<5:
+                    print("**Length should be at least 5. Try again.")
+                    num_valid=True
+                else:
+                    num_valid=False
+            except ValueError:
+                print("**Invalid input. Use numbers.")
+                num_valid=True
+            
+        sys_password="".join(random.sample(cls.password_chars, k=pass_length))
         pyperclip.copy(sys_password)               
         return sys_password
 
