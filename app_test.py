0c70d2aff184ebd820a5a86887787fa4cbd246b8
collin9726 <owino.collin@gmail.com>
2020-05-23 15:08:08 +0300

    add tests for sys password generation and copying to clipboard

--- app_test.py
+++ app_test.py
@@ -1,6 +1,7 @@
 import unittest
 import pyperclip
 from account import Account
+from password import Password
 
 class AppTest(unittest.TestCase):
     def setUp(self):
@@ -12,7 +13,14 @@ class AppTest(unittest.TestCase):
     def test_account_init(self):
         self.assertEqual(self.account_obj.acc_nm, "Twitter")
         self.assertEqual(self.account_obj.acc_uname, "collin")
-        self.assertEqual(self.account_obj.acc_pass, "collin123")        
+        self.assertEqual(self.account_obj.acc_pass, "collin123")
+
+    def test_gen_password(self):
+        pass_length=int(input("Test sys password length: "))
+        self.assertEqual(len(Password.gen_password()), pass_length)
+
+    def test_gen_password_copy(self):
+        self.assertEqual(Password.gen_password(), pyperclip.paste())        
 
 
 if __name__=='__main__':
